8b3e48bf5fa41c87e1252c0574f400aacfc0365d
tomichau <t.chaumette@orange.fr>
2023-03-09 20:44:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,17 +14,24 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <!--Fontawesome-->
-        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     </head>
 
     <body>
         <div class="container site">
             <h1 class="text-logo"><span class="glyphicon glyphicon-cutlery"></span> Burger Code <span class="glyphicon glyphicon-cutlery"></span></h1>
-            
+            <div class="panier">
+                <a class="basket" href="#"><span class="fa-solid fa-basket-shopping"></span></a>
+                <div class="point">
+                    <div class="point-item">
+                    <p>0</p>
+                    </div>
+                </div>
+            </div>
             <?php 
                 require 'admin/database.php';
-			 
+                $styleWidthHeight = "";
                 echo '<nav>
                         <ul class="nav nav-pills">';
 
@@ -57,9 +64,15 @@
                     $statement->execute(array($category['id']));
                     while ($item = $statement->fetch()) 
                     {
+                        if($item['category'] == 5){
+                            $styleWidthHeight = "width:500px;height:400px";
+                        }
+                        else{
+                            $styleWidthHeight = "width:500px;height:300px";
+                        }
                         echo '<div class="col-sm-6 col-md-4">
                                 <div class="thumbnail">
-                                    <img src="images/' . $item['image'] . '" alt="...">
+                                    <img src="images/' . $item['image'] . '" alt="'.$item["image"].'" style="'.$styleWidthHeight.'">
                                     <div class="price">' . number_format($item['price'], 2, '.', ''). ' €</div>
                                     <div class="caption">
                                         <h4>' . $item['name'] . '</h4>
